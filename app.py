c470f7e13d850041ca0336ff671ebdf71f4a5bdc
sinboleto <santiago@sinboleto.com.mx>
2023-06-29 20:38:17 -0600

    Agrega logica de las FAQs y las variables de ambiente

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+from dotenv import load_dotenv
 import os
+from unidecode import unidecode
 
 app = Flask(__name__)
 
 port = int(os.environ.get('PORT', 5000))
 
-# Twilio credentials
-account_sid = 'ACf01ddcd618830097852506cba7b428ef'
-auth_token = 'a6626c30c4a87f2f44a6df6d483b8d7b'
+# Load environment variables from .env file
+load_dotenv()
 
-# twilio_phone_number = '+15416923070'
-twilio_phone_number = '+14155238886' # Sandbox
+# Twilio credentials
+account_sid = os.environ.get('ACCOUNT_SID')
+auth_token = os.environ.get('AUTH_TOKEN')
+twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
 
 # Create Twilio client
 client = Client(account_sid, auth_token)
@@ -21,20 +24,66 @@ client = Client(account_sid, auth_token)
 @app.route('/', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
-        incoming_message = request.values.get('Body', '').lower()
+        incoming_message = unidecode(request.values.get('Body', '').lower())
         response = MessagingResponse()
 
+        lista_saludo = ['hola','buenos dias','buenas tardes','buenas noches']
+        lista_FAQ_1 = ['cuando','fecha','hora','donde','direccion','duracion']
+        lista_FAQ_2 = ['hospedaje']
+        lista_FAQ_3 = ['estacionamiento','valet parking']
+        lista_FAQ_4 = ['dress code','codigo vestimenta','vestimenta']
+        lista_FAQ_5 = ['agenda']
+
         # Check if the incoming message is a frequently asked question
-        if incoming_message == 'hi':
-            response.message("Hello! How can I assist you today?")
-        elif incoming_message == 'how are you?':
-            response.message("I'm a chatbot. I don't have feelings, but thanks for asking!")
-        elif incoming_message == 'what is your name?':
-            response.message("I'm a chatbot. You can call me ChatGPT!")
+        if incoming_message in lista_saludo:
+            response.message("Hola ¿Cómo te puedo ayudar?")
+        
+        elif incoming_message in lista_FAQ_1:
+            respuesta_FAQ_1 = """Esta es la información general del evento:
+            Fecha: 08 de agosto de 2023
+            Cita: 19:00 hrs
+            Venue: Foro Codere
+            Dirección: Av. Industria Militar s/n esq. Periférico. Col. Lomas de Sotelo,
+            Del. Miguel Hidalgo. Link: https://goo.gl/maps/AiDZWCWrWLwViW817
+            Acceso a estacionamiento y a venue por puerta 1 y 2 del Hipódromo de las Américas"""
+            response.message(respuesta_FAQ_1)
+        
+        elif incoming_message in lista_FAQ_2:
+            respuesta_FAQ_2 = """Contamos con alizanza con el hotel Hyatt Regency Mexico City Insurgentes.
+            Link: https://goo.gl/maps/HCqTAzWbhSrvB58NA.
+            Para reservaciones contactar a: asistente@amdk.mx.
+            Habrá transportación del hotel a Foro Codere"""
+            response.message(respuesta_FAQ_2)
+        
+        elif incoming_message in lista_FAQ_3:
+            respuesta_FAQ_3 = """El venue cuenta con estacionamiento privado y servicio de valet parking"""
+            response.message(respuesta_FAQ_3)
+        
+        elif incoming_message in lista_FAQ_4:
+            respuesta_FAQ_4 = """El dress code es smart business. Te proponemos algunas opciones:
+            - Combinación de prendas
+            - Camisas lisas, corbata opcional
+            - Tejidos como la lana, el tweed y el algodón
+            - Blazers con falda, vestidos por la rodilla, camisas lisas o pantalones de sastre
+            - Joyas sencillas y lisas, si se llevan
+            - Cinturones y zapatos cerrados pulidos
+            - Maletines, bolsos de mano o elegantes
+            La paleta de colores suele ser de tonos neutros como el negro, el azul marino, el gris, el beige y el crema"""
+            response.message(respuesta_FAQ_4)
+
+        elif incoming_message in lista_FAQ_5:
+            respuesta_FAQ_5 = """El itinerario del evento será el siguiente:
+            19:00 - Registro
+            20:00 - Bienvenida
+            20:15 - Presentación
+            21:15 - Cena"""
+            response.message(respuesta_FAQ_5)
 
         # If it's not a frequently asked question, escalate the conversation to a human agent
         else:
             # forward_to_agent(incoming_message)
+            response.message("""Para responder a su pregunta a continuación, un asesor lo atenderá por este medio. 
+            Gracias por su paciencia""")
             pass
 
         return str(response)
